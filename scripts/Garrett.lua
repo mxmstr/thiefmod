251fa11ecdddecc0905b8e3ec7d1d9808d49013e
Eric Lynch <lynch.er18@gmail.com>
2018-09-19 21:10:09 -0400

    init

--- scripts/Garrett.lua
+++ scripts/Garrett.lua
@@ -1,13 +1,36 @@
 local ui = lgs.DarkUISrv
 local property = lgs.PropertySrv
 local link = lgs.LinkSrv
+local hook = lgs.DarkHookSrv
 
 
 function BeginScript(msg)
 
+    hook.DarkHookInitialize()
+    hook.InstallPropHook(msg.to, true, 'Position', msg.to)
+
+
     property.Add(msg.to, 'SelfLit')
-    property.Set(msg.to, 'SelfLit', 100)
-    ui.TextMessage(tostring(property.possessed(msg.to, 'SelfLit')))
+    print(property.Set(msg.to, 'SelfLit', 0))
+    --ui.TextMessage(tostring(property.possessed(msg.to, 'SelfLit')))
+
+    return true
+
+end
+
+
+function PhysicsMessages(msg)
+
+    print('AAAAAAAAAAAAAAAAAAAAAAA')--tostring(msg.to)..tostring(mgs.from))
+
+    return true
+
+end
+
+
+function DHNotify(msg)
+
+    --print()
 
     return true
 
