9508ca0ffc5e80f7d95d9c60346c139782482df3
Eric Lynch <lynch.er18@gmail.com>
2018-09-23 20:39:24 -0400

    motion

--- scripts/Garrett.lua
+++ scripts/Garrett.lua
@@ -2,17 +2,35 @@ local ui = lgs.DarkUISrv
 local property = lgs.PropertySrv
 local link = lgs.LinkSrv
 local hook = lgs.DarkHookSrv
+local round = math.round
+local square = math.sqrt
+
+local last_pos = nil
+
+
+
+local GetDistance = function(a, b)
+
+    local num1 = round(a.x, 0)
+    local num2 = round(b.x, 0)
+    --num1 = num1 + round(a.x)
+    ui.TextMessage(a.x - b.x)
+    --ui.TextMessage( tostring( + b.x) )
+
+    local x, y, z = a.x - b.x, a.y - b.y, a.z - b.z
+    return square(x * x + y * y + z * z)
+
+end
 
 
 function BeginScript(msg)
 
-    hook.DarkHookInitialize()
-    hook.InstallPropHook(msg.to, true, 'Position', msg.to)
+    --hook.DarkHookInitialize()
+    print(hook.InstallPropHook(msg.to, true, 'Position', msg.to))
 
 
     property.Add(msg.to, 'SelfLit')
-    print(property.Set(msg.to, 'SelfLit', 0))
-    --ui.TextMessage(tostring(property.possessed(msg.to, 'SelfLit')))
+    property.Set(msg.to, 'SelfLit', 0)
 
     return true
 
@@ -30,7 +48,19 @@ end
 
 function DHNotify(msg)
 
-    print(tostring(property.Get(msg.to, 'Position')))
+    local new_pos = property.Get(msg.to, 'Position')
+
+    
+    print('asdf')
+
+    if last_pos ~= nil then
+        print(tostring(new_pos))
+        print(tostring(last_pos))
+        --local dist = GetDistance(new_pos, last_pos)
+        --ui.TextMessage(tostring(dist))
+    end
+
+    last_pos = new_pos
 
     return true
 
