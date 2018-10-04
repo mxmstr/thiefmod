9c464ec00cea079cf98699014bfc5cc6c76de443
Eric Lynch <lynch.er18@gmail.com>
2018-10-03 20:48:29 -0400

    Staring

---
+++ scripts/AI.lua
@@ -0,0 +1,38 @@
+
+local ui = lgs.DarkUISrv
+local property = lgs.PropertySrv
+
+
+function BeginScript(msg)
+
+    ui.TextMessage(tostring(property.Remove(msg.to, 'M-AlertCapZero')))
+
+    script:SetTimedMessage('name', 16, 'Periodic', 'Update')
+
+    return true
+
+end
+
+
+function Custom(msg)
+
+    --ui.TextMessage('asdf')
+    return true
+
+end
+
+
+function Timer(msg)
+
+    if msg.data[1] == 'Update' then
+        Update()
+    end
+
+end
+
+
+function Update()
+
+
+
+end
